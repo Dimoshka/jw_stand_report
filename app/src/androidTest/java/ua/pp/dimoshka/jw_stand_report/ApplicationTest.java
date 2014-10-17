33560a738f6cbb28dbc06baeb3193c9125608d9d
dimoshka_usb <dimoshka.ua@gmail.com>
2014-10-17 15:52:55 +0300

    Initial commit

---
+++ app/src/androidTest/java/ua/pp/dimoshka/jw_stand_report/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ua.pp.dimoshka.jw_stand_report;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
